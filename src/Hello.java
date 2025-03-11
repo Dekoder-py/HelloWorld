61e712df79675cf88b47b249ba6df3ad0b1926ca
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-11 17:16:36 +1300

    add if then statement into a code block

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
         System.out.println("Hello, Dekoder-py");
 
         boolean isAlien = false;
-        if (isAlien == false)
+        if (isAlien == false) { // Can be simplified to !isALien but leaving as is to follow the example
             System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens!");
+        }
     }
 }
