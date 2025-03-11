b9e9490a3924bdcb934c236ac0fab7e4ac0a1ed7
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-11 21:03:16 +1300

    fixed a bug where the remainder boolean was incorrect

--- src/PrecedenceChallenge.java
+++ src/PrecedenceChallenge.java
@@ -4,9 +4,8 @@ public class PrecedenceChallenge {
         double secondDouble = 80.00d;
         double stepThree = (firstDouble + secondDouble) * 100;
         double stepFour = stepThree % 40;
-        boolean remainder = (stepFour == 0) ? false : true;
-        System.out.println(remainder);
-        if (!remainder) {
+        boolean noRemainder = (stepFour == 0) ? true : false;
+        if (!noRemainder) {
             System.out.println("got some remainder");
         }
     }
